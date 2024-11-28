e13dfd3eb09b4a8bbad3582a790f45303a314f6e
jarotrivera <jarotriverasoto@gmail.com>
2024-11-28 00:46:04 -0300

    commit Final Railway 3

--- src/views/VistaHacerUnPost.jsx
+++ src/views/VistaHacerUnPost.jsx
@@ -17,19 +17,19 @@ const VistaHacerUnPost = () => {
       if (!token) {
         throw new Error('Usuario no autenticado');
       }
-  
+
       // Decodificar el token para obtener el usuarioId
       const tokenPayload = JSON.parse(atob(token.split('.')[1]));
       const usuarioId = tokenPayload.id; // Obtén el usuarioId del token
-  
+
       const nuevoPost = {
         titulo,
         foto,
         descripcion,
         usuarioId, // Agregar usuarioId a la solicitud
       };
-  
-      const response = await fetch('https://forogeocentro-production.up.railway.app/api/posts', {
+
+      const response = await fetch('http://localhost:3000/api/posts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -37,26 +37,25 @@ const VistaHacerUnPost = () => {
         },
         body: JSON.stringify(nuevoPost),
       });
-  
+
       if (!response.ok) {
         throw new Error('Error al crear la publicación');
       }
-  
+
       const data = await response.json();
       console.log('Publicación creada:', data);
-  
+
       // Limpiar los campos del formulario y mostrar el mensaje de éxito
       setTitulo('');
       setFoto('');
       setDescripcion('');
       setMensajeExito('Publicación creada con éxito');
-  
+
       setTimeout(() => setMensajeExito(''), 3000);
     } catch (error) {
       console.error('Error:', error);
     }
   };
-  
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
@@ -72,43 +71,42 @@ const VistaHacerUnPost = () => {
       <main className="content">
         <Sidebar />
         <section className="main-panel">
-          <div className="form-wrapper">
-            <div className="form-container">
-              <form onSubmit={handleSubmit} className="post-form">
-                <div className="form-group">
-                  <label>Título:</label>
-                  <input
-                    type="text"
-                    value={titulo}
-                    onChange={(e) => setTitulo(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Foto:</label>
-                  <input
-                    type="file"
-                    accept="image/*"
-                    onChange={handleFileChange}
-                  />
-                  {foto && (
-                    <div className="photo-preview">
-                      <img src={foto} alt="Vista previa" />
-                    </div>
-                  )}
-                </div>
-                <div className="form-group">
-                  <label>Descripción:</label>
-                  <textarea
-                    value={descripcion}
-                    onChange={(e) => setDescripcion(e.target.value)}
-                  />
-                </div>
-                <button type="submit">Agregar Publicación</button>
-              </form>
-              {/* Mostrar mensaje de éxito */}
-              {mensajeExito && <p className="mensaje-exito">{mensajeExito}</p>}
-            </div>
+          <div className="formulario-container">
+            <form onSubmit={handleSubmit} className="formulario">
+              <div className="form-group">
+                <label>Título:</label>
+                <input
+                  type="text"
+                  value={titulo}
+                  onChange={(e) => setTitulo(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label>Foto:</label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleFileChange}
+                />
+                {foto && (
+                  <div className="photo-preview">
+                    <img src={foto} alt="Vista previa" />
+                  </div>
+                )}
+              </div>
+              <div className="form-group">
+                <label>Descripción:</label>
+                <textarea
+                  value={descripcion}
+                  onChange={(e) => setDescripcion(e.target.value)}
+                  required
+                />
+              </div>
+              <button type="submit">Agregar Publicación</button>
+            </form>
+            {/* Mostrar mensaje de éxito */}
+            {mensajeExito && <p className="mensaje-exito">{mensajeExito}</p>}
           </div>
         </section>
         <RightPanel2 />
--- src/views/VistaHacerUnaVenta.jsx
+++ src/views/VistaHacerUnaVenta.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Sidebar from "../components/Menu";
-import RightPanel2 from "../components/RightPanel2"; // Importar el RightPanel2 sin modificar estilos
-import Menu from "../components/Menu"; // Importar el componente Menu
+import RightPanel2 from "../components/RightPanel2";
 import "./VistaHacerUnaVenta.css";
 
 const VistaHacerUnaVenta = () => {
@@ -30,7 +29,7 @@ const VistaHacerUnaVenta = () => {
       }
 
       const nuevaVenta = { titulo, descripcion, precio, foto };
-      const response = await fetch('https://forogeocentro-production.up.railway.app/api/ventas', {
+      const response = await fetch('http://localhost:3000/api/ventas', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -56,66 +55,53 @@ const VistaHacerUnaVenta = () => {
 
   return (
     <div className="vista-hacer-una-venta">
-      <div className="sidebar-sticky">
+      <main className="content">
         <Sidebar />
-      </div>
-
-      <div className="menu-sticky">
-        <Menu />
-      </div>
-
-      <div className="content2">
         <section className="main-panel">
-          <div className="form-wrapper">
-            <div className="form-container">
-              <form className="post-form" onSubmit={handleSubmit}>
-                <div className="form-group">
-                  <label>Título</label>
-                  <input
-                    type="text"
-                    value={titulo}
-                    onChange={(e) => setTitulo(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Descripción</label>
-                  <textarea
-                    value={descripcion}
-                    onChange={(e) => setDescripcion(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Precio</label>
-                  <input
-                    type="number"
-                    value={precio}
-                    onChange={(e) => setPrecio(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="form-group">
-                  <label>Foto</label>
-                  <input type="file" accept="image/*" onChange={handleFileChange} />
-                  {foto && (
-                    <div className="photo-preview">
-                      <img src={foto} alt="Vista previa" />
-                    </div>
-                  )}
-                </div>
-                <button type="submit">Publicar Venta</button>
-              </form>
-              {mensajeExito && <p className="mensaje-exito">{mensajeExito}</p>}
-            </div>
+          <div className="formulario-container">
+            <form className="formulario" onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label>Título</label>
+                <input
+                  type="text"
+                  value={titulo}
+                  onChange={(e) => setTitulo(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label>Descripción</label>
+                <textarea
+                  value={descripcion}
+                  onChange={(e) => setDescripcion(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label>Precio</label>
+                <input
+                  type="number"
+                  value={precio}
+                  onChange={(e) => setPrecio(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label>Foto</label>
+                <input type="file" accept="image/*" onChange={handleFileChange} />
+                {foto && (
+                  <div className="photo-preview">
+                    <img src={foto} alt="Vista previa" />
+                  </div>
+                )}
+              </div>
+              <button type="submit">Publicar Venta</button>
+            </form>
+            {mensajeExito && <p className="mensaje-exito">{mensajeExito}</p>}
           </div>
         </section>
-
-        {/* RightPanel2 importado sin que se estire */}
-        <div className="right-panel-fixed">
-          <RightPanel2 />
-        </div>
-      </div>
+        <RightPanel2 />
+      </main>
     </div>
   );
 };
