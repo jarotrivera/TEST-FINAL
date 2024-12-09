698b4046af456689c2a185d9653394bde7454977
jarotrivera <jarotriverasoto@gmail.com>
2024-12-08 21:35:37 -0300

    Primer commit

--- backend/routes/parkingRoutes.js
+++ backend/routes/parkingRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const { actualizarEstadoEstacionamiento, obtenerEstadoEstacionamiento } = require('../controllers/parkingController');
 const router = express.Router();
 
+// Ruta para actualizar el estado del estacionamiento (sin autenticación temporalmente)
 router.post('/update', actualizarEstadoEstacionamiento);
+
+// Ruta para obtener el estado del estacionamiento (ya funcional)
 router.get('/', obtenerEstadoEstacionamiento);
 
 module.exports = router;
